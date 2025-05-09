4a0610482cb9efee9bf165ccca7ae5e24d70871b
Github <visionh789@gmail.com>
2025-05-09 11:40:22 +0100

    Create serveur.js

---
+++ serveur.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route de test
+app.get('/', (req, res) => {
+  res.send('Backend du site vitrine opérationnel !');
+});
+
+// Lancer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
